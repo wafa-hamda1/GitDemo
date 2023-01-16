6eb733c699cb1598772be07f5d3e2015460c3cbc
len <hamdawafa2016@gmail.com>
2023-01-16 09:54:13 +0100

    Architect Asian development

---
+++ src/SeleniumTest.java
@@ -0,0 +1,15 @@
+
+public class SeleniumTest {
+	
+	public void IOSApp()
+	{
+		System.out.println("selenium test");
+		
+	}
+
+	public void postJira()
+	{
+		System.out.println("Native App");
+		
+	}
+}
